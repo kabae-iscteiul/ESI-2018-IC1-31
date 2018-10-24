112e7e04d90ae8f11e88d6277b62a734c03137e3
ijcoo-iscteiul <ijcoo@iscte-iul.pt>
2018-10-24 16:59:57 +0100

    Actualização do codigo do twitter

--- Bom dia academia/src/Codigo/Twitter.java
+++ Bom dia academia/src/Codigo/Twitter.java
@@ -5,34 +5,41 @@ import java.util.List;
 import twitter4j.Status;
 //import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.api.TweetsResources;
 import twitter4j.conf.ConfigurationBuilder;
 public class Twitter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		
 		
-		
-		try {
+	/*	try {
         	ConfigurationBuilder cb = new ConfigurationBuilder();
         	cb.setDebugEnabled(true)
-        	  .setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
-        	  .setOAuthConsumerSecret("zKH2yAtRyefwsgOO8h8Szc4kru68iEm95QmIG7svw")
-        	  .setOAuthAccessToken("36481851-VhzByC4f9MSsZES1QZQ4e4iBvA9bWGLyv9HKFpy7c")
-        	  .setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
+        	  .setOAuthConsumerKey("2yLs0GI3Rs3V9o9ncHGLEg3OL")
+        	  .setOAuthConsumerSecret("SgRRr42fc1W80pQuKzgCwC286z2teCN17UHCnyhWG15oRDuixM")
+        	  .setOAuthAccessToken("1052650934398898176-vgbhRssh1LLXR8hqfRXTN4q5d6eOeA")
+        	  .setOAuthAccessTokenSecret("EA7fvBtU7GTIf2Q5ksIgOpbuuPFPRzyX9T5IZiGiBXO3p");
         	TwitterFactory tf = new TwitterFactory(cb.build());
         	twitter4j.Twitter twitter = tf.getInstance();        		
             List<Status> statuses = twitter.getHomeTimeline();
+            
+            
             System.out.println("------------------------\n Showing home timeline \n------------------------");
-    		int counter=0;
+    
+        twitter4j.Twitter tw=tf.getInstance();
+            Status stat= tw.updateStatus("kkk");
+           
+         
+            
+            int counter=0;
     		int counterTotal = 0;
             for (Status status : statuses) {
             	
 				// Filters only tweets from user "catarina"
-            	
-            	// Temos que colocar os nossos Twitters aqui **********
-            	
-				if (status.getUser().getName() != null && status.getUser().getName().contains("catarina")) {
+  	
+				if (status.getUser().getName() != null && status.getUser().getName().contains("Iran Carimo")) {
 					System.out.println(status.getUser().getName() + ":" + status.getText());
 					counter++;
 				}
@@ -40,7 +47,11 @@ public class Twitter {
             }
     		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
         } catch (Exception e) { System.out.println(e.getMessage()); }
+        
+        
+        
+        */
 
 	}
+	}
 
-}
--- Bom dia academia/src/GUI/Tela_2.java
+++ Bom dia academia/src/GUI/Tela_2.java
@@ -6,11 +6,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import twitter4j.Status;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.Color;
 
 public class Tela_2 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField10;
+	private JLabel lblDigiteOSeu;
+	private JButton btnListarTodosTweets;
 
 	/**
 	 * Launch the application.
@@ -36,11 +51,126 @@ public class Tela_2 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblJanela = new JLabel("JANELA 2");
-		contentPane.add(lblJanela, BorderLayout.NORTH);
+		JButton btnNewButton = new JButton("Twettar");
+		btnNewButton.setBackground(Color.CYAN);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			
+			
+				try {
+		        	ConfigurationBuilder cb = new ConfigurationBuilder();
+		        	cb.setDebugEnabled(true)
+		        	  .setOAuthConsumerKey("2yLs0GI3Rs3V9o9ncHGLEg3OL")
+		        	  .setOAuthConsumerSecret("SgRRr42fc1W80pQuKzgCwC286z2teCN17UHCnyhWG15oRDuixM")
+		        	  .setOAuthAccessToken("1052650934398898176-vgbhRssh1LLXR8hqfRXTN4q5d6eOeA")
+		        	  .setOAuthAccessTokenSecret("EA7fvBtU7GTIf2Q5ksIgOpbuuPFPRzyX9T5IZiGiBXO3p");
+		        	TwitterFactory tf = new TwitterFactory(cb.build());
+		        	twitter4j.Twitter twitter = tf.getInstance();        		
+		            List<Status> statuses = twitter.getHomeTimeline();
+		            System.out.println("------------------------\n Showing home timeline \n------------------------");
+		    		
+		            //twitter4j.Twitter tw=tf.getInstance();
+		            
+		          
+		           twitter4j.Twitter tw=tf.getInstance();
+		            Status stat= tw.updateStatus(textField10.getText());
+		            
+		            
+		            
+		            //Status stat = ((TweetsResources) tf).updateStatus(" Welcome to my twitter");
+		            
+		            int counter=0;
+		    		int counterTotal = 0;
+		            for (Status status : statuses) {
+		            	
+						// Filters only tweets from user "catarina"
+		            	
+		            	// Temos que colocar os nossos Twitters aqui **********
+		            	
+						if (status.getUser().getName() != null && status.getUser().getName().contains("Iran Carimo")) {
+							System.out.println(status.getUser().getName() + ":" + status.getText());
+							counter++;
+						}
+						counterTotal++;
+		            }
+		    		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+		        } catch (Exception e) { System.out.println(e.getMessage()); }
+			
+			
+			
+			
+			
+			}
+		});
+		btnNewButton.setBounds(158, 127, 151, 25);
+		contentPane.add(btnNewButton);
+		
+		textField10 = new JTextField();
+		textField10.setBounds(40, 72, 324, 22);
+		contentPane.add(textField10);
+		textField10.setColumns(10);
+		
+		lblDigiteOSeu = new JLabel("Digite o seu Tweet aqui");
+		lblDigiteOSeu.setBounds(126, 43, 163, 16);
+		contentPane.add(lblDigiteOSeu);
+		
+		btnListarTodosTweets = new JButton("Listar todos Tweets");
+		btnListarTodosTweets.addActionListener(new ActionListener() {
+		
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				try {
+		        	ConfigurationBuilder cb = new ConfigurationBuilder();
+		        	cb.setDebugEnabled(true)
+		        	  .setOAuthConsumerKey("2yLs0GI3Rs3V9o9ncHGLEg3OL")
+		        	  .setOAuthConsumerSecret("SgRRr42fc1W80pQuKzgCwC286z2teCN17UHCnyhWG15oRDuixM")
+		        	  .setOAuthAccessToken("1052650934398898176-vgbhRssh1LLXR8hqfRXTN4q5d6eOeA")
+		        	  .setOAuthAccessTokenSecret("EA7fvBtU7GTIf2Q5ksIgOpbuuPFPRzyX9T5IZiGiBXO3p");
+		        	TwitterFactory tf = new TwitterFactory(cb.build());
+		        	twitter4j.Twitter twitter = tf.getInstance();        		
+		            List<Status> statuses = twitter.getHomeTimeline();
+		            System.out.println("------------------------\n Showing home timeline \n------------------------");
+		    		
+		            //twitter4j.Twitter tw=tf.getInstance();
+		            
+		          
+		  
+		            
+		            
+		            //Status stat = ((TweetsResources) tf).updateStatus(" Welcome to my twitter");
+		            
+		            int counter=0;
+		    		int counterTotal = 0;
+		            for (Status status : statuses) {
+		            	
+						// Filters only tweets from user "catarina"
+		            	
+		            	// Temos que colocar os nossos Twitters aqui **********
+		            	
+						if (status.getUser().getName() != null && status.getUser().getName().contains("Iran Carimo")) {
+							System.out.println(status.getUser().getName() + ":" + status.getText());
+							counter++;
+						}
+						counterTotal++;
+		            }
+		    		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+		        } catch (Exception e) { System.out.println(e.getMessage()); }
+
+				
+				
+				
+			
+			
+			
+			
+			}
+		});
+		btnListarTodosTweets.setBounds(158, 161, 151, 25);
+		contentPane.add(btnListarTodosTweets);
 	}
 
 }
